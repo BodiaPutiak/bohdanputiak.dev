a32e986c37e14c2dbab87fd9007cd0860487ecf4
BodiaPutiak <121493849+BodiaPutiak@users.noreply.github.com>
2024-02-23 13:38:43 +0100

    small fixes

--- personal-website/src/components/Portfolio/index.jsx
+++ personal-website/src/components/Portfolio/index.jsx
@@ -18,8 +18,8 @@ const Portfolio = () => {
         spotifyMusicController: {name: 'SpotifyMusicController', index: 2, title: 'Spotify Music Controller', img: spotifyMc, imgClass: '', stack: ['HTML','Material UI', 'ReactJS', 'Django', 'Python']},
         GymBro: {name: 'GymBro', index: 3, title: 'GymBro', img: GymBro, imgClass: 'gym', stack: ['HTML', 'SCSS', 'ReactJS']},
         EcommerceStore: {name: 'EcommerceStore', index: 4, title: 'Minimalist Ecommerce Store', img: EcommerceStore, imgClass: 'ecomm', stack: ['HTML', 'SCSS', 'ReactJS']},
-        BookMark: {name: 'BookMark', index: 5, title: 'BookMark Langing Page', img: BookMark, imgClass: 'ecomm', stack: ['HTML', 'SCSS', 'ReactJS']},
-        CarRental: {name: 'CarRental', index: 6, title: 'Website for car rental service', img: CarRental, imgClass: 'gym', stack: ['HTML', 'SCSS', 'ReactJS']}
+        BookMark: {name: 'BookMark', index: 5, title: 'BookMark Langing Page', img: BookMark, imgClass: 'book', stack: ['HTML', 'SCSS', 'ReactJS']},
+        CarRental: {name: 'CarRental', index: 6, title: 'Website for car rental service', img: CarRental, imgClass: 'rental', stack: ['HTML', 'SCSS', 'ReactJS']}
     };
     const location = useLocation();
     const { projectName } = useParams();
